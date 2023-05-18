74f6b61742c3111db4aecf798685088a24350656
eakinloose <eakinloose@gmail.com>
2023-05-18 11:26:28 +0100

    firebase setup for web and mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/web_screen_layout.dart';
@@ -5,7 +7,21 @@ import 'package:instagram/utils/colors.dart';
 
 import 'package:instagram/responsive/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDXLfNuKnomm0x0x9F0HKUr7WvC3U061Vs",
+        appId: "1:197543978111:web:1fbf726633b870e4614b4a",
+        messagingSenderId: "197543978111",
+        projectId: "instagram-61ec8",
+        storageBucket: "instagram-61ec8.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
